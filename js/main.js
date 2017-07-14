067e54aa210b814714cbc9fa5bbc979e3b91f57c
narfpoit <moongold81@gmail.com>
2017-07-14 13:11:56 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -26,7 +26,9 @@ var cards = [
 ];
 var cardsInPlay = [];
 
+
 var checkForMatch = function() {
+
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
       alert("You found a match!");
@@ -35,8 +37,9 @@ var checkForMatch = function() {
     }
   }
 }
-var flipCard = function(cardId) {
-
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id');
+  this.setAttribute('src', cards[cardId].cardImage);
   console.log("User flipped " + cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
@@ -44,5 +47,14 @@ var flipCard = function(cardId) {
   checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
+createBoard();
